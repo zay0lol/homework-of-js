07749603e1cd6ef2bd45894a6ad2aa7a917791dc
zay0lol <zynahisthebest2012@gmail.com>
2024-09-22 22:11:38 +0500

    intro to js homework

---
+++ script.js
@@ -0,0 +1,10 @@
+var num1=4
+var num2=9
+var num3=3
+var num4=8
+var sum = num1 + num2;
+document.getElementById("calc"). innerHTML = "The total sum is:"
++sum
+var sum = num3 + num4;
+document.getElementById("hi"). innerHTML = "The answer is:"
++sum
